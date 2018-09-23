054fef8fcfd4d1950a18e9f858d83fbba8d47207
Eowyn Baughman <eowyn42@gmail.com>
2018-09-23 12:29:59 -0700

    Get the pig latinizer working!

--- main.py
+++ main.py
@@ -19,7 +19,23 @@ def get_fact():
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    # Get a random fact as a string
+    fact = get_fact()
+
+    # Send the request to the pig latinizer
+    urlReq = "https://hidden-journey-62459.herokuapp.com/piglatinize/"
+    payload = {'input_text': fact}
+    post_req = requests.post(urlReq, data=payload, allow_redirects=False)
+
+    # Get the url where the response will come from
+    urlResponse = post_req.headers['Location']
+
+    # Get the response and pull the pig latin string from the headers
+    response = requests.get(urlResponse)
+    soup = BeautifulSoup(response.content, "html.parser")
+    pigs = soup.find_all('h2')
+
+    return pigs[0].nextSibling.strip()
 
 
 if __name__ == "__main__":
